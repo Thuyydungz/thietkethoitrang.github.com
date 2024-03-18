094268a60aacbd49d7f6acde4a99a3e2ef9d4fad
leminhthanh142 <thanhle12345789@gmail.com>
2024-03-18 22:21:30 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const items = document.querySelectorAll(".cartegory-right-content-item")
 items.forEach((item, index) => {
   item.addEventListener("click", () => {
-    window.location.href = `./detail.html?id=${index+1}`
+    window.location.href = `../pages/detail.html?id=${index+1}`
   })
 })
